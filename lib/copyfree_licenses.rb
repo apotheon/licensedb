2c77f8ce6f8ac4abe1defce21e458195c9213e31
ren <code@apotheon.net>
2017-01-19 02:32:52 +0000

    use orcish method

--- lib/copyfree_licenses.rb
+++ lib/copyfree_licenses.rb
@@ -2,17 +2,11 @@ require 'sequel'
 require 'net/http'
 
 class CopyfreeLicenses
-  attr_accessor :db
-
-  def initialize
-    @db = {
-      mit: {
-        name: 'MIT License'
-      }
-    }
-  end
-
   def license_names
     db.values.map {|license| license[:name] }
+  end
+
+  def db
+    @db ||= { mit: { name: 'MIT License' } }
   end
 end
