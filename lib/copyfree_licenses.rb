4f712c960c1afefc29b2ebffb61afec6dcac3438
ren <code@apotheon.net>
2017-03-05 03:39:08 +0000

    add COIL fixture

--- lib/copyfree_licenses.rb
+++ lib/copyfree_licenses.rb
@@ -1,13 +1,54 @@
 require 'sequel'
 
 class CopyfreeLicenses
-  def license_names
-    db.values.map {|license| license[:name] }
+  def abbrevs
+    db.map {|license| license[:abbrev] }
+  end
+
+  def names
+    db.map {|license| license[:name] }
+  end
+
+  def texts
+    db.map {|license| license[:text] }
+  end
+
+  def add_license license_data
+    db << license_data
+  end
+
+  def get_by_abbrev abbreviation
+    begin
+      db[abbrevs.find_index abbreviation]
+    rescue TypeError => e
+      { abbrev: nil, name: nil, text: nil, error: 'no such license' }
+    end
+  end
+
+  def get_by_name name
+    begin
+      db[names.find_index name]
+    rescue TypeError => e
+      { abbrev: nil, name: nil, text: nil, error: 'no such license' }
+    end
+  end
+
+  def get_by_text text
+    begin
+      db[texts.find_index text]
+    rescue TypeError => e
+      { abbrev: nil, name: nil, text: nil, error: 'no such license' }
+    end
   end
 
   private
 
   def db
-    @db ||= { mit: { name: 'MIT License' } }
+    @db ||= [
+      {
+        abbrev: 'MIT',
+        name: 'MIT License'
+      }
+    ]
   end
 end
--- test/copyfree_licenses_test.rb
+++ test/copyfree_licenses_test.rb
@@ -1,13 +1,67 @@
 require 'minitest/spec'
+require 'minitest/must_not'
 require 'minitest/autorun'
 require 'minitest/rg'
 
 require '../lib/copyfree_licenses.rb'
 
+require './helpers.rb'
+
 describe CopyfreeLicenses do
+  abbrev = 'COIL'
+  name = 'Copyfree Open Innovation License'
+  text = fixture_file('coil.txt')
+
+  license_data = {
+    abbrev: abbrev,
+    name: name,
+    text: text
+  }
+
+  no_license_error = {
+    abbrev: nil,
+    name: nil,
+    text: nil, 
+    error: 'no such license'
+  }
+
   CopyfreeLicenses.new.tap do |cl|
     it 'returns a list of copyfree license names' do
-      cl.license_names.must_include 'MIT License'
+      cl.names.must_include 'MIT License'
+      cl.names.must_not_include name
+    end
+
+    it 'returns a list of copyfree license abbreviations' do
+      cl.abbrevs.must_include 'MIT'
+      cl.abbrevs.must_not_include abbrev
+    end
+
+    it 'does not find a nonexistent license' do
+      cl.get_by_abbrev(abbrev).must_equal no_license_error
+      cl.get_by_name(name).must_equal no_license_error
+      cl.get_by_text(text).must_equal no_license_error
+    end
+  end
+
+  CopyfreeLicenses.new.tap do |cl|
+    before do
+      cl.add_license license_data
+    end
+
+    it 'adds a license to the database and returns license texts' do
+      cl.texts.must_include text
+    end
+
+    it 'gets a license by abbreviation' do
+      cl.get_by_abbrev(abbrev).must_equal license_data
+    end
+
+    it 'gets a license by name' do
+      cl.get_by_name(name).must_equal license_data
+    end
+
+    it 'gets a license by text' do
+      cl.get_by_text(text).must_equal license_data
     end
   end
 end
